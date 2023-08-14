b5bb86517f0ae8dcaabba8c2789c182bd1df9966
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-08-14 16:40:29 +0200

    feat(app): 21. Get started with AWS Amplify

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Amplify } from "aws-amplify";
+
 import Navigator from "./src/navigation";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
